46935af4dd22f20f28f353a77eee1d7d2b800f98
Daniel LittleThunder <daniel.littlethunder@moxiesozo.com>
2025-01-13 12:45:17 -0700

    Removed contact form

--- client/src/components/home/SectionContact.vue
+++ client/src/components/home/SectionContact.vue
@@ -1,14 +1,14 @@
 <template>
   <section id="section__contact" class="page-section">
     <div class="section-row section-row--fullwidth flex-wrap">
-      <div class="section-col section-col--6">
+      <div class="section-col">
         <div class="section-content">
           <SectionTitle title="Contact Me" />
           <h3 class="text-clr-blue">Available For Hire</h3>
           <p class="text-clr-400 dark:text-clr-200">Interested in having me as part of your team? Send me an email at <a href="mailto:hello@codemints.io" class="underline text-clr-blue hover:text-clr-100 dark:text-clr-orange dark:hover:text-clr-400">hello@codemints.io</a>. Thank you for visiting my portfolio. I hope you enjoyed it.</p>
         </div>
       </div>
-      <div
+      <!-- <div
         @mouseover="buttonsStore.toggleCursors(true)"
         @mouseout="buttonsStore.toggleCursors(false)"
         class="section-col section-col--6"
@@ -16,7 +16,7 @@
         <div class="section-content">
           <Form />
         </div>
-      </div>
+      </div> -->
     </div>
   </section>
 </template>
